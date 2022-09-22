270c1623afe87203cf90846f089a9615adb13c63
Sonali-Mali93 <sonalimali999@gmail.com>
2022-09-22 14:09:27 +0530

    feat: logic is wrote for adding and getting polygon data

--- src/post-gis-backend/post-gis-backend.service.ts
+++ src/post-gis-backend/post-gis-backend.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { from, Observable } from 'rxjs';
 import { Repository } from 'typeorm';
-import { CreatePostGisBackendDto } from './dto/create-post-gis-backend.dto';
-import { UpdatePostGisBackendDto } from './dto/update-post-gis-backend.dto';
+import { Parcel } from './entities/polygon.entity';
 import { PostGisBackend } from './entities/post-gis-backend.entity';
 import { PostGis } from './entities/postgis.interface';
-
+// import { polygonEntity } from './entities/polygon.entity';
 @Injectable()
 export class PostGisBackendService {
   constructor(
     @InjectRepository(PostGisBackend)
     private readonly PostGisDataRepository: Repository<PostGisBackend>,
+    @InjectRepository(Parcel)
+    private readonly parcel: Repository<Parcel>,
   ) {}
   create(postGisData: PostGis): Observable<PostGis> {
     return from(this.PostGisDataRepository.save(postGisData));
@@ -19,6 +20,28 @@ export class PostGisBackendService {
 
   findAll() {
     return from(this.PostGisDataRepository.find());
+  }
+
+
+  async createParcel(createParcelPointDto: Parcel): Promise<any> {
+    console.log('this is come from dto ', createParcelPointDto);
+    const polygon = {
+      type: 'polygon',
+      coordinates: [createParcelPointDto.coordinates],
+      City_Name: createParcelPointDto.cityname,
+    };
+    console.log('polygon variable data', polygon);
+    // const parcel = this.parcel.create({ polygon })
+    const parcel = this.parcel.create({
+      cityname: createParcelPointDto.cityname,
+      polygon,
+    });
+    console.log(parcel);
+    await this.parcel.save(parcel);
+    return parcel;
+  }
+  findAllPolygon() {
+    return from(this.parcel.find());
   }
 
   // findOne(id: number) {
