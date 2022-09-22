d863d796649b2302a81b13eb397d50701a4c8549
Sonali-Mali93 <sonalimali999@gmail.com>
2022-09-22 14:15:14 +0530

    feat: api for adding and getting polygon data

--- src/post-gis-backend/post-gis-backend.module.ts
+++ src/post-gis-backend/post-gis-backend.module.ts
@@ -3,12 +3,12 @@ import { PostGisBackendService } from './post-gis-backend.service';
 import { PostGisBackendController } from './post-gis-backend.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostGisBackend } from './entities/post-gis-backend.entity';
+import { Parcel } from './entities/polygon.entity';
+// import { polygonEntity } from './entities/polygon.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([PostGisBackend])
-  ],
+  imports: [TypeOrmModule.forFeature([PostGisBackend, Parcel])],
   controllers: [PostGisBackendController],
-  providers: [PostGisBackendService]
+  providers: [PostGisBackendService],
 })
 export class PostGisBackendModule {}
