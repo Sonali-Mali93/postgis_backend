3ad9d7c92c733ba609bd6c6439c5a4c831e9f69f
Sonali-Mali93 <sonalimali999@gmail.com>
2022-09-26 12:49:48 +0530

    feat: linestring is injected to service

--- src/post-gis-backend/post-gis-backend.module.ts
+++ src/post-gis-backend/post-gis-backend.module.ts
@@ -4,10 +4,13 @@ import { PostGisBackendController } from './post-gis-backend.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostGisBackend } from './entities/post-gis-backend.entity';
 import { Parcel } from './entities/polygon.entity';
+import { linestringentity } from './entities/linestring.entity';
 // import { polygonEntity } from './entities/polygon.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostGisBackend, Parcel])],
+  imports: [
+    TypeOrmModule.forFeature([PostGisBackend, Parcel, linestringentity]),
+  ],
   controllers: [PostGisBackendController],
   providers: [PostGisBackendService],
 })
--- src/post-gis-backend/post-gis-backend.service.ts
+++ src/post-gis-backend/post-gis-backend.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { from, Observable } from 'rxjs';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
+import { linestringentity } from './entities/linestring.entity';
 import { Parcel } from './entities/polygon.entity';
 import { PostGisBackend } from './entities/post-gis-backend.entity';
 import { PostGis } from './entities/postgis.interface';
+import { MultiLineString } from 'geojson';
+
 // import { polygonEntity } from './entities/polygon.entity';
 @Injectable()
 export class PostGisBackendService {
@@ -13,6 +16,8 @@ export class PostGisBackendService {
     private readonly PostGisDataRepository: Repository<PostGisBackend>,
     @InjectRepository(Parcel)
     private readonly parcel: Repository<Parcel>,
+    @InjectRepository(linestringentity)
+    private readonly linestrings: Repository<linestringentity>,
   ) {}
   create(postGisData: PostGis): Observable<PostGis> {
     return from(this.PostGisDataRepository.save(postGisData));
@@ -21,7 +26,6 @@ export class PostGisBackendService {
   findAll() {
     return from(this.PostGisDataRepository.find());
   }
-
 
   async createParcel(createParcelPointDto: Parcel): Promise<any> {
     console.log('this is come from dto ', createParcelPointDto);
@@ -40,10 +44,32 @@ export class PostGisBackendService {
     await this.parcel.save(parcel);
     return parcel;
   }
+
   findAllPolygon() {
     return from(this.parcel.find());
   }
 
+  async createLinePoint(
+    createLinePointDto: linestringentity,
+  ): Promise<linestringentity> {
+    console.log('this is come from linestring service ', createLinePointDto);
+    const { coordinates } = createLinePointDto;
+    const lineString: MultiLineString = {
+      type: 'MultiLineString',
+      coordinates: [coordinates],
+    };
+    console.log('line no-73', lineString);
+    const line = this.linestrings.create({
+      lineString,
+      City_Name: createLinePointDto.City_Name,
+    });
+    await this.linestrings.save(line);
+    return line;
+  }
+
+  lineString() {
+    return from(this.linestrings.find());
+  }
   // findOne(id: number) {
   //   return `This action returns a #${id} postGisBackend`;
   // }
